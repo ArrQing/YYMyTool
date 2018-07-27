f210c49dcfa4d58940628556c5c9ce4c55497f40
YY <3297509752@qq.com>
2018-07-27 17:39:28 +0800

    -【add】倒计时

--- YYMyTool.podspec
+++ YYMyTool.podspec
@@ -3,7 +3,7 @@
 Pod::Spec.new do |s|
 
   s.name         = 'YYMyTool'
-  s.version      = '1.0.0'
+  s.version      = '1.0.1'
   s.summary      = 'An easy way to send request and upload file'
   s.homepage     = 'https://github.com/yy32/YYMyTool'
   s.license      = 'MIT'
---
+++ YYTool/YYTool/YYMyManagerTool.h
@@ -0,0 +1,20 @@
+//
+//  YYMyManagerTool.h
+//  YYTool
+//
+//  Created by admin on 2018/7/27.
+//  Copyright © 2018年 ArrQ. All rights reserved.
+//
+
+#ifndef YYMyManagerTool_h
+#define YYMyManagerTool_h
+
+//网络请求
+#import "VDRequest.h"
+
+// 倒计时
+#import "YYTimeManager.h"
+
+
+
+#endif /* YYMyManagerTool_h */
